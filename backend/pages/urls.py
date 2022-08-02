b112f0c6ef5f3ec86e3db87f79523e8bca8235da
Zach-EE <zachholloway.n@gmail.com>
2022-08-02 17:31:47 +0000

    have vue app set up and running in django static template page

--- backend/pages/urls.py
+++ backend/pages/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import IndexPageView, HomePageView
 
 urlpatterns = [
-    path('index', IndexPageView.as_view(), name='index'),
-    path('', HomePageView.as_view(), name='home'),
+    path('', IndexPageView.as_view(), name='index'),
+    path('home', HomePageView.as_view(), name='home'),
 ]
